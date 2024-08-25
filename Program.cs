ccd0adfa06e97d85a0c786584bcd40839c7fc0a5
Alejandro Castañeda <alejandro@MacBook-Pro-de-Alejandro.local>
2024-08-25 13:07:47 -0600

    arreglos

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("¿Cuántos trabajadores tienes?");
+        int agregar = int.Parse(Console.ReadLine());
+
+      
+        string[] prac = new string[agregar];
+
+        for (int i = 0; i < prac.Length; i++)
+        {
+            Console.WriteLine("Ingrese el nombre del trabajador número " + (i + 1) + ":");
+            prac[i] = Console.ReadLine();
+        }
+
+    
+        Console.WriteLine("Los nombres ingresados son:");
+        for (int i = 0; i < prac.Length; i++)
+        {
+            Console.WriteLine(prac[i]);
+        }
+    }
+}
+
